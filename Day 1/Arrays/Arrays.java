4515a66f2afae4bcccfd02362ac314c36382f4bc
Don Tran <donqt15@gmail.com>
2022-07-05 19:22:29 -0700

    Day1 changes I didn't do lmao

--- Day 1/Arrays/Arrays.java
+++ Day 1/Arrays/Arrays.java
@@ -2,6 +2,11 @@ package Arrays;
 
 public class Arrays {
   public static void main (String[] args) {
-    
+    String[] list = {"Tech", "Rohin Sood", "Optix", "Joe Byedon"};
+
+    System.out.println(list[3]);
+    System.out.println(list[2]);
+    System.out.println(list[1]);
+    System.out.println(list[0]);
   }
 }
---
+++ Day 1/Variables/Variables.java
@@ -0,0 +1,17 @@
+package Variables;
+
+public class Variables {
+  public static void main (String[] args) {
+    int age = 16;
+    double height = 5.67;
+    char firstLetter = 'D';
+    String lastName = "Tran";
+    boolean programmingBest = true;
+
+    System.out.println(age);
+    System.out.println(height);
+    System.out.println(firstLetter);
+    System.out.println(lastName);
+    System.out.println(programmingBest);
+  }
+}
